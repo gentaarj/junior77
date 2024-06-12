9630e0c50fb8a30afe72ea6d38c6b1c8256c6f5f
gentaarj <167460018+gentaarj@users.noreply.github.com>
2024-06-13 01:02:23 +0700

    Update bot.sh

--- Bot/bot.sh
+++ Bot/bot.sh
@@ -96,86 +96,86 @@ systemctl enable cybervpn
 clear
 echo "downloading asset"
 
-wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/messiey/rocky/master/botssh/panelbot.sh" && chmod +x /usr/bin/panelbot
+wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/panelbot.sh" && chmod +x /usr/bin/panelbot
 
 
 
 
 
-wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/messiey/rocky/master/botssh/addnoobz.sh" && chmod +x /usr/bin/addnoobz
+wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/addnoobz.sh" && chmod +x /usr/bin/addnoobz
 
 
 
-wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/messiey/rocky/master/botssh/log-install.txt"
+wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/log-install.txt"
 
 
 
-wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/messiey/rocky/master/botssh/add-vless.sh" && chmod +x /usr/bin/add-vless
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/add-vless.sh" && chmod +x /usr/bin/add-vless
 
-wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/messiey/rocky/master/botssh/addtr.sh" && chmod +x /usr/bin/addtr
-
-
-wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/messiey/rocky/master/botssh/addws.sh" && chmod +x /usr/bin/addws
+wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/addtr.sh" && chmod +x /usr/bin/addtr
 
 
-wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/messiey/rocky/master/botssh/addss.sh" && chmod +x /usr/bin/addss
-
-wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
+wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/addws.sh" && chmod +x /usr/bin/addws
 
 
+wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/addss.sh" && chmod +x /usr/bin/addss
 
-wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-ss.sh" && chmod +x /usr/bin/cek-ss
+wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
 
 
 
-
-wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-tr.sh" && chmod +x /usr/bin/cek-tr
-
-wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-vless.sh" && chmod +x /usr/bin/cek-vless
-
-wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-ws.sh" && chmod +x /usr/bin/cek-ws
-
-wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/messiey/rocky/master/botssh/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-ss.sh" && chmod +x /usr/bin/cek-ss
 
 
 
 
-wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
+wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-tr.sh" && chmod +x /usr/bin/cek-tr
+
+wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-vless.sh" && chmod +x /usr/bin/cek-vless
+
+wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-ws.sh" && chmod +x /usr/bin/cek-ws
+
+wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/del-vless.sh" && chmod +x /usr/bin/del-vless
 
 
 
-wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/messiey/rocky/master/botssh/deltr.sh" && chmod +x /usr/bin/deltr
 
-wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/messiey/rocky/master/botssh/delws.sh" && chmod +x /usr/bin/delws
-
-
-
-wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/messiey/rocky/master/botssh/delss.sh" && chmod +x /usr/bin/delss
+wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
 
 
 
-wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/messiey/rocky/master/botssh/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/deltr.sh" && chmod +x /usr/bin/deltr
 
-
-wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/messiey/rocky/master/botssh/renewtr.sh" && chmod +x /usr/bin/renewtr
-
-
-wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/messiey/rocky/master/botssh/renewvless.sh" && chmod +x /usr/bin/renewvless
-
-
-wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/messiey/rocky/master/botssh/renewws.sh" && chmod +x /usr/bin/renewws
-
-
-wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-mws.sh" && chmod +x /usr/bin/cek-mws
-
-wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
-
-
-wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/delws.sh" && chmod +x /usr/bin/delws
 
 
 
-wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/messiey/rocky/master/botssh/cek-mts.sh" && chmod +x /usr/bin/cek-mts
+wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/delss.sh" && chmod +x /usr/bin/delss
+
+
+
+wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+
+
+wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/renewtr.sh" && chmod +x /usr/bin/renewtr
+
+
+wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/renewvless.sh" && chmod +x /usr/bin/renewvless
+
+
+wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/renewws.sh" && chmod +x /usr/bin/renewws
+
+
+wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-mws.sh" && chmod +x /usr/bin/cek-mws
+
+wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
+
+
+wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+
+
+
+wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/gentaarj/junior77/main/Bot/cek-mts.sh" && chmod +x /usr/bin/cek-mts
 
 cp /tmp/var.txt /media/cybervpn
 
